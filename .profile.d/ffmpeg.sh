e080e4c9c05259822feb0cb1010daf521d9595b8
vetleen <vetleengesbak@gmail.com>
2026-04-10 17:35:43 +0200

    Add blas/lapack lib paths to LD_LIBRARY_PATH for ffprobe (WILFRED-3F)
    The apt buildpack installs libblas and liblapack into subdirectories
    (blas/, lapack/) that aren't on LD_LIBRARY_PATH. Add all three
    missing subdirectory paths in one go.

--- .profile.d/ffmpeg.sh
+++ .profile.d/ffmpeg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Add PulseAudio library path so ffprobe/ffmpeg can find libpulsecommon
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/.apt/usr/lib/x86_64-linux-gnu/pulseaudio"
+# Add library subdirectories the apt buildpack doesn't put on LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/.apt/usr/lib/x86_64-linux-gnu/pulseaudio:/app/.apt/usr/lib/x86_64-linux-gnu/blas:/app/.apt/usr/lib/x86_64-linux-gnu/lapack"
